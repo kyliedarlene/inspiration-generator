485edbd5c96f10d187283560788e8fba03a2c1be
Kylie Clark <kyliedarlene@icloud.com>
2024-08-28 17:34:31 -0700

    adds router

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,23 @@ import React from "react";
 import App from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/Home";
+import RandomCharacter from "./pages/RandomCharacter";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/random-character',
+        element: <RandomCharacter />
+    }
+])
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+// root.render(<App />);
+root.render(<RouterProvider router={router} />);
