f29ee2d8aa65d4689082e8e013839d3b6507f2d9
Kylie Clark <kyliedarlene@icloud.com>
2024-08-29 16:53:16 -0700

    adds /characters POST

--- server/app.py
+++ server/app.py
@@ -17,14 +17,42 @@ from models import *
 def index():
     return '<h1>Inspiration Generator API</h1>'
 
-@app.route('/characters')
+@app.route('/characters', methods = ['GET', 'POST'])
 def characters():
     characters = [character.to_dict() for character in Character.query.all()]
 
-    response = make_response(
-        characters,
-        200
-    )
+    if request.method == 'GET':
+        response = make_response(
+            characters,
+            200
+        )
+    elif request.method == 'POST':
+        try: 
+            form_data = request.get_json()
+
+            new_character = Character()
+            for attr in dir(Character):
+                if attr in form_data:
+                    setattr(new_character, attr, form_data[attr])
+
+            db.session.add(new_character)
+            db.session.commit()
+
+            response = make_response(
+                new_character.to_dict(),
+                201
+            )
+        except ValueError as e:
+            response = make_response(
+                { "errors": [str(e)] }, 
+                400
+            )
+        except: ## improvement (low-priority): make non-Value error messages more informative 
+                    # How to catch IntegrityError for NULL constraint violation?
+            response = make_response(
+                { "errors": ['Please try again.'] }, 
+                400
+            )
 
     return response
 
