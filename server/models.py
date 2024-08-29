c1412091894634ea2492fc0fdfc729ce1dfad89e
Kylie Clark <kyliedarlene@icloud.com>
2024-08-29 15:48:54 -0700

    creates Character model

--- server/models.py
+++ server/models.py
@@ -3,4 +3,14 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Character(db.Model, SerializerMixin):
+    __tablename__ = 'characters'
+
+    id = db.Column(db.Integer, primary_key=True)
+    race_name = db.Column(db.String)
+    cls_name = db.Column(db.String)
+    cls_desc = db.Column(db.String)
+    arch_name = db.Column(db.String)
+    arch_desc = db.Column(db.String)
+    bkd_name = db.Column(db.String)
+    bkd_desc = db.Column(db.String)
