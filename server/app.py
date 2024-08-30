790013385955d5780b329cfdeacb86b0214492a0
Kylie Clark <kyliedarlene@icloud.com>
2024-08-29 19:35:21 -0700

    cleans up notes

--- server/app.py
+++ server/app.py
@@ -9,14 +9,9 @@ from werkzeug.exceptions import NotFound
 
 # Local imports
 from config import app, db, api
-# Add your model imports
 from models import *
 
-# Views go here!
-
-# @app.route('/')
-# def index():
-#     return '<h1>Inspiration Generator API</h1>'
+##### VIEWS #####
 
 class Home(Resource):
 
@@ -32,6 +27,7 @@ class Home(Resource):
 api.add_resource(Home, '/')
 
 ### authentication ###
+
 
 
 ### characters ###
@@ -67,44 +63,6 @@ class Characters(Resource):
         return response
     
 api.add_resource(Characters, '/characters')
-
-# @app.route('/characters', methods = ['GET', 'POST'])
-# def characters():
-#     characters = [character.to_dict() for character in Character.query.all()]
-
-#     if request.method == 'GET':
-#         response = make_response(
-#             characters,
-#             200
-#         )
-#     elif request.method == 'POST':
-#         try: 
-#             form_data = request.get_json()
-
-#             new_character = Character()
-#             for attr in dir(Character):
-#                 if attr in form_data:
-#                     setattr(new_character, attr, form_data[attr])
-
-#             db.session.add(new_character)
-#             db.session.commit()
-
-#             response = make_response(
-#                 new_character.to_dict(),
-#                 201
-#             )
-#         except ValueError as e:
-#             response = make_response(
-#                 { "errors": [str(e)] }, 
-#                 400
-#             )
-#         except: ## improvement (low-priority): make non-Value error messages more informative 
-#                     # How to catch IntegrityError for NULL constraint violation?
-#             response = make_response(
-#                 { "errors": ['Please try again.'] }, 
-#                 400
-#             )
-#     return response
 
 class CharacterByID(Resource):
 
@@ -156,52 +114,6 @@ class CharacterByID(Resource):
         return response
 
 api.add_resource(CharacterByID, '/characters/<int:id>')
-
-
-# @app.route('/characters/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
-# def character_by_id(id):
-#     character = Character.query.filter(Character.id == id).first()
-
-#     if not character:
-#         response = make_response(
-#             {"error": "Character not found"}, 
-#             404
-#         )
-#     else:
-#         if request.method == 'GET':
-#             response = make_response(
-#                 character.to_dict(),
-#                 200
-#             )
-#         elif request.method == 'PATCH':
-#             try:
-#                 form_data = request.get_json()
-
-#                 for attr in form_data:
-#                     setattr(character, attr, form_data[attr])
-
-#                 db.session.commit()
-#                 response = make_response(
-#                     character.to_dict(), 
-#                     202
-#                 )
-#             except ValueError:
-#                 response = make_response(
-#                     { "errors": ["validation errors"] }, 
-#                     400
-#                 )
-#         elif request.method == 'DELETE':
-#             db.session.delete(character)
-#             db.session.commit()
-#             response = make_response(
-#                 { 
-#                     "delete_successful": True, 
-#                     "message": "Character deleted." 
-#                 },
-#                 200
-#             )
-
-#     return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
