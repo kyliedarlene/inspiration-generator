260f8e9b1e66aea51425aa52600382666a73bbdb
Kylie Clark <kyliedarlene@icloud.com>
2024-08-31 14:15:13 -0700

    adds password and username elements, still static + no onSubmit

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,23 +4,42 @@ import * as yup from "yup";
 function Signup() {
 
     const formSchema = yup.object().shape({
-        email: yup.string().email("Invalid email").required("Must enter email"),
-      });
+        username: yup
+            .string(),
+        email: yup
+            .string()
+            .email("Invalid email")
+            .required("Must enter email"),
+        password: yup
+            .string()
+    });
 
       const formik = useFormik({
         initialValues: {
-          email: "",
+            username: "",
+            email: "",
+            password: "",
         },
         validationSchema: formSchema,
         onSubmit: (values) => {
             console.log("Submitted")
         },
-      });
+      })
 
       return (
         <div>
-          <h1>Signup Form</h1>
+          <h1>Signup</h1>
           <form onSubmit={formik.handleSubmit} style={{ margin: "30px" }}>
+
+            <label htmlFor="username">Username</label>
+            <br />
+            <input
+              id="username"
+              name="username"
+              onChange={formik.handleChange}
+              value={formik.values.username}
+            />
+            <p style={{ color: "red" }}> {formik.errors.email}</p>
 
             <label htmlFor="email">Email</label>
             <br />
@@ -31,6 +50,16 @@ function Signup() {
               value={formik.values.email}
             />
             <p style={{ color: "red" }}> {formik.errors.email}</p>
+
+            <label htmlFor="password">Password</label>
+            <br />
+            <input
+              id="password"
+              name="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+            />
+            <p style={{ color: "red" }}> {formik.errors.password}</p>
             
             <button type="submit">Submit</button>
           </form>
