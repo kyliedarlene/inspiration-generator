17bb51f672f105f1c79d5e9f4e29be29f0bef87b
Kylie Clark <kyliedarlene@icloud.com>
2024-08-30 18:10:31 -0700

    moves error handling to bottom of page

--- server/app.py
+++ server/app.py
@@ -24,20 +24,6 @@ class Home(Resource):
     
 api.add_resource(Home, '/')
 
-### error handling ###
-
-@app.errorhandler(NotFound)
-def handle_not_found(e):
-
-    response = make_response(
-        "Not Found: The requested resource does not exist.",
-        404
-    )
-
-    return response
-
-app.register_error_handler(404, handle_not_found)
-
 ### authentication ###
 
 class Signup(Resource):
@@ -241,6 +227,20 @@ class CharacterById(Resource):
         return response
 
 api.add_resource(CharacterById, '/characters/<int:id>')
+
+### error handling ###
+
+@app.errorhandler(NotFound)
+def handle_not_found(e):
+
+    response = make_response(
+        "Not Found: The requested resource does not exist.",
+        404
+    )
+
+    return response
+
+app.register_error_handler(404, handle_not_found)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
