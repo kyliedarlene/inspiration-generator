b3bebd636ecd0d1da34e763a51e4d5523ab4ac9f
Kylie Clark <kyliedarlene@icloud.com>
2024-09-01 15:17:41 -0700

    cleans up imports

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,9 @@
+import { useEffect, useContext } from "react";
 import { Outlet } from "react-router-dom";
 
 import Header from "./Header";
 
 import { UserContext } from "../context/user";
-import { useEffect, useContext } from "react";
 
 function App() {
   const {user, checkSession} = useContext(UserContext)
