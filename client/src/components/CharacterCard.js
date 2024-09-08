217ecf7374317338438004b0984a37004285fe5f
Kylie Clark <kyliedarlene@icloud.com>
2024-09-07 18:17:39 -0700

    rough static char card

--- client/src/components/CharacterCard.js
+++ client/src/components/CharacterCard.js
@@ -1,16 +1,63 @@
+import { Fragment } from 'react'
+import {
+    Box,
+    Card,
+    CardActions,
+    CardContent,
+    IconButton,
+    Button,
+    Typography
+} from '@mui/material'
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+
 function CharacterCard({ character }) {
 
-    return (
-        <>
-            <h3>Character Card</h3>
-            <h4>Race: {character.race_name}</h4>
-            <h4>Class: {character.cls_name}</h4>
-            <h4>Subclass: {character.arch_name ? character.arch_name : null} </h4>
-            <p>{character.arch_desc ? character.arch_desc : null} </p>
-            <h4>Backround: {character.bkd_name}</h4>
-            <p>{character.bkd_desc}</p>
-        </>
+    const card = (
+        <Fragment>
+            
+            <CardContent>
+                
+                <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+                    {character.race_name}
+                </Typography>
+
+                <Typography variant="h5" component="div">
+                {character.arch_name ? character.arch_name : null} {character.cls_name}
+                </Typography>
+
+                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
+                    {character.bkd_name}
+                </Typography>
+
+            </CardContent>
+
+            <CardActions>
+
+            <IconButton aria-label="delete">
+                <StarBorderIcon />
+            </IconButton>
+
+            </CardActions>
+
+        </Fragment>
     )
+
+    return(
+        <Box sx={{ minWidth: 275 }}>
+            <Card variant="outlined">{card}</Card>
+        </Box>
+    )
+
+    // return (
+    //     <>
+    //         <h4>Race: {character.race_name}</h4>
+    //         <h4>Class: {character.cls_name}</h4>
+    //         <h4>Subclass: {character.arch_name ? character.arch_name : null} </h4>
+    //         <p>{character.arch_desc ? character.arch_desc : null} </p>
+    //         <h4>Backround: {character.bkd_name}</h4>
+    //         <p>{character.bkd_desc}</p>
+    //     </>
+    // )
 }
 
 export default CharacterCard;
