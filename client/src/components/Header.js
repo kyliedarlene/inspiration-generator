6c26a15372328e70392b4ef1830cfdd096cde25c
Kylie Clark <kyliedarlene@icloud.com>
2024-09-07 17:20:24 -0700

    logout onClick functional

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,20 +10,107 @@ import {
     Typography,
     Button,
     IconButton,
+    Menu,
     MenuItem,
+    Switch,
+    FormGroup,
+    FormControlLabel
 } from '@mui/material'
 import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
     const { user, logout } = useContext(UserContext)
     const location = useLocation();
     const navigate = useNavigate();
+
+    /////////////
+
+    const [auth, setAuth] = useState(true);
     const [anchorEl, setAnchorEl] = useState(null);
-
-
-    const handleProfileMenuOpen = (event) => {
+  
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+  
+    const handleMenu = (event) => {
         setAnchorEl(event.currentTarget);
-      };
+    };
+  
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+
+        {/* auth toggle */}
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={auth}
+              onChange={handleChange}
+              aria-label="login switch"
+            />
+          }
+          label={auth ? 'Logout' : 'Login'}
+        />
+      </FormGroup>
+
+      {/* nav bar */}
+      <AppBar position="static">
+        <Toolbar>
+
+          {/* title */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Inspiration Generator
+          </Typography>
+
+          {/* render if logged in */}
+          {auth && (
+            <div>
+
+              {/* profile icon */}
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+
+              {/* profile dropdown */}
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClick={logout}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+
+    }
 
     // improvement: something more legible than a nested ternary
 
@@ -48,42 +135,5 @@ function Header() {
     //         </header>
     //     </>
     // )
-
-    return(
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" >
-                <Toolbar>
-
-                {/* Title */}
-                <Typography 
-                    variant="h6" 
-                    component="div" 
-                    align="center"
-                    sx={{ flexGrow: 1 }}
-                >
-                    Inspiration Generator
-                </Typography>
-
-                {/* Button: View Saved Characters */}
-                <Button color="inherit">View Saved Characters</Button>
-
-                {/* Button: Profile dropdown, option to logout */}
-                <MenuItem>
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="primary-search-account-menu"
-                        aria-haspopup="true"
-                        color="inherit"
-                    >
-                        <AccountCircle />
-                    </IconButton>
-                </MenuItem>
-                
-                </Toolbar>
-            </AppBar>
-        </Box>
-    )
-}
 
 export default Header; 
