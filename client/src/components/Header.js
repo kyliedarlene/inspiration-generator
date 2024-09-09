b086e0e2f4a592c05c59b91b4cef75d68f7dbb43
Kylie Clark <kyliedarlene@icloud.com>
2024-09-08 21:03:13 -0700

    refactors nav elements into individual variables

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,114 +2,127 @@ import React, { useContext, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import {
-    AppBar,
-    Box,
-    Toolbar,
-    Typography,
-    Button,
-    IconButton,
-    Menu,
-    MenuItem,
-    Switch,
-    FormGroup,
-    FormControlLabel
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
 } from '@mui/material'
 import AccountCircle from '@mui/icons-material/AccountCircle';
 
 import { UserContext } from "../context/user";
 
 function Header() {
-    const { user, logout } = useContext(UserContext)
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    const [anchorEl, setAnchorEl] = useState(null);
+  const { user, logout } = useContext(UserContext)
+  const location = useLocation();
+  const navigate = useNavigate();
   
-    const handleMenu = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+  const [anchorEl, setAnchorEl] = useState(null);
   
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
 
+  const appTitle = (
+    <Link to='/'>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Inspiration Generator
+      </Typography>
+    </Link>
+  )
+
+  const rollMoreCharsButton = (
+    <Link to={'/random-character'}>
+      <Button color="inherit" >Roll more characters!</Button>
+    </Link>
+  );
+
+  const myCharsButton = (
+    <Link to={'/saved'}>
+      <Button color="inherit" >My characters</Button>
+    </Link>
+  )
+
+  const loginButton = <Button color="inherit" onClick={() => navigate('/login')} >Login</Button>;
+  
   return (
+    
     <Box sx={{ flexGrow: 1 }}>
-
+      
       {/* nav bar */}
       <AppBar position="static">
         <Toolbar>
-
+          
           {/* title */}
-          <Link to='/'>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                Inspiration Generator
-            </Typography>
-          </Link>
+          {appTitle}
 
           {/* render if logged in */}
-          {user && (
-            <div>
-                
-                {/* Button: view saved characters */}
-                {location.pathname === '/saved' ? 
-                    <Link to={'/random-character'}><Button color="inherit" >Roll more characters!</Button></Link> 
-                    : 
-                    <Link to={'/saved'}><Button color="inherit" >My characters</Button></Link> 
-                }
+            {user && (
+              <div>
+                  
+                  {/* Button: view saved characters */}
+                  {location.pathname === '/saved' ? 
+                      rollMoreCharsButton
+                      : 
+                      myCharsButton
+                  }
 
-                {/* profile icon */}
-                <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleMenu}
-                    color="inherit"
-                >
-                    <AccountCircle />
-                </IconButton>
+                  {/* profile icon */}
+                  <IconButton
+                      size="large"
+                      aria-label="account of current user"
+                      aria-controls="menu-appbar"
+                      aria-haspopup="true"
+                      onClick={handleMenu}
+                      color="inherit"
+                  >
+                      <AccountCircle />
+                  </IconButton>
 
-                {/* profile dropdown */}
-                <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                    }}
-                    open={Boolean(anchorEl)}
-                    onClick={logout}
-                    onClose={handleClose}
-                >
-                    <MenuItem onClick={handleClose}>Logout</MenuItem>
-                </Menu>
+                  {/* profile dropdown */}
+                  <Menu
+                      id="menu-appbar"
+                      anchorEl={anchorEl}
+                      anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                      }}
+                      open={Boolean(anchorEl)}
+                      onClick={logout}
+                      onClose={handleClose}
+                  >
+                      <MenuItem onClick={handleClose}>Logout</MenuItem>
+                  </Menu>
 
-            </div>
-          )}
+              </div>
+            )}
 
-          {/* render if NOT logged in and route !== /login or /signup */}
-          {!user && location.pathname !== '/login' && location.pathname !== '/signup' && (
-            <div>
+            {/* render if NOT logged in and route !== /login or /signup */}
+            {!user && location.pathname !== '/login' && location.pathname !== '/signup' && (
+              <div>
+                  {loginButton}
 
-                {/* Button: login */}
-                <Button color="inherit" onClick={() => navigate('/login')} >
-                    Login
-                </Button>
+              </div>
+            )}
 
-            </div>
-          )}
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
 
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+  }
 
-}
-
-export default Header; 
+  export default Header; 
