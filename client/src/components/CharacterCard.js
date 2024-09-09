df46c6499592fe8eac45f31f1282da660fab2e4a
Kylie Clark <kyliedarlene@icloud.com>
2024-09-09 12:22:19 -0700

    conditional rendering for null card (refactor later)

--- client/src/components/CharacterCard.js
+++ client/src/components/CharacterCard.js
@@ -1,12 +1,10 @@
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
 import {
     Box,
     Card,
     CardActions,
     CardContent,
     CardMedia,
-    IconButton,
-    Button,
     Typography
 } from '@mui/material'
 
@@ -15,39 +13,55 @@ import classIcons from '../assets/class_icons/classIcons'
 
 function CharacterCard({ character }) {
 
-    const card = (
+    console.log(character)
+
+    let card = (
         <Fragment>
-            <CardActions>
-                <SaveCharacter character={character} />
-            </CardActions>
-
-            <CardMedia
-                sx={{ height: 140 }}
-                component="img"
-                image={classIcons[character['cls_name']]}
-                title="class icon"
-                alt="class"
-            />
-
             <CardContent>
                 <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-                    {character.race_name}
-                </Typography>
-
-                <Typography variant="h5" component="div">
-                {character.arch_name ? character.arch_name : null} {character.cls_name}
-                </Typography>
-
-                <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
-                    {character.bkd_name}
+                    rolling character...
                 </Typography>
             </CardContent>
         </Fragment>
     )
 
+    if(character) {
+        card = (
+            <Fragment>
+                <CardActions>
+                    <SaveCharacter character={character} />
+                </CardActions>
+    
+                <CardMedia
+                    sx={{ height: 140 }}
+                    component="img"
+                    image={classIcons[character['cls_name']]}
+                    title="class icon"
+                    alt="class"
+                />
+    
+                <CardContent>
+                    <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
+                        {character.race_name}
+                    </Typography>
+    
+                    <Typography variant="h5" component="div">
+                    {character.arch_name ? character.arch_name : null} {character.cls_name}
+                    </Typography>
+    
+                    <Typography sx={{ color: 'text.secondary', mb: 1.5 }}>
+                        {character.bkd_name}
+                    </Typography>
+                </CardContent>
+            </Fragment>
+        )
+    }
+
     return(
         <Box sx={{ maxWidth: 300 }}>
-            <Card align={'center'} sx={{height: 360}} variant="outlined">{card}</Card>
+            <Card align={'center'} sx={{height: 360}} variant="outlined">
+                {card}
+            </Card>
         </Box>
     )
 }
